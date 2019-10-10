7c2b9b17b48e0188e8f6283adacf7b4acd4509f1
Pedja <vpedja12@gmail.com>
2019-10-10 13:55:23 -0700

    Layout of page completed. Javascript moved to seperate sheet.

---
+++ function.js
@@ -0,0 +1,8 @@
+var sec = 75
+var wrongAns = 15
+
+$(document).ready(function () {
+
+    // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
+    // ...
+    $("#start-button").on("click", function () {

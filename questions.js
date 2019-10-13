140dc6badc36c4d4487354f878dcb9e983c332f6
Pedja <vpedja12@gmail.com>
2019-10-12 23:33:08 -0700

    Readme pictures updated.

--- questions.js
+++ questions.js
@@ -1,27 +1,26 @@
 var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "2"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "2"
-  },
-  {
-  title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "2"
-  },
-  {
-  title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "2"
-  },
-  {
-  title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "2"
-  },
-];
+    {
+		title: "Which is not an ingredient in a Harvey Wallbanger cocktail?", 
+		choices: ["Orange Juice", "Vodka", "Sour Mix", "Galliano" ],
+		answer: 2,
+	}, 
+	{
+		title: "How many items are there in a Bakers' Dozen?", 
+		choices: ["12", "6", "24", "13" ],
+		answer: 3,
+	}, 
+	{
+		title: "What is the most widely eaten fish in the world?", 
+		choices: ["Tilapia", "Herring", "Sardine", "Tuna" ],
+		answer: 1,
+	}, 
+	{
+		title: "Which fruit does not ripen once it has been picked?", 
+		choices: ["Banana", "Lemon", "Mango", "Apple" ],
+		answer: 1,
+	}, 
+	{
+		title: "Which fruit contains the most protein per 100 calories?", 
+		choices: ["Guava", "Avocado", "Banana", "Blackberries" ],
+		answer: 0,
+	}];

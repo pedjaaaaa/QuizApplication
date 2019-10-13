3c36647057d5a006cbfbb5fb35fc7e5473740c43
Pedja <vpedja12@gmail.com>
2019-10-12 18:22:21 -0700

    Created timeline and initial screenshot.

--- function.js
+++ function.js
@@ -1,8 +1,15 @@
-var sec = 75
-var wrongAns = 15
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+$(document).ready(function(){
 
-$(document).ready(function () {
-
-    // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
-    // ...
     $("#start-button").on("click", function () {
+        alert("The quiz has started!");
+        $(".mainsec").text("New text");
+
+    })
+
+
+});
+//var sec = 75;
+//var wrongAns = 15;
+//var startTime = 0;
+//var currentScore = 0; 
